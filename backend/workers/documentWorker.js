30301114c054090755f67def7cfea50114b881d6
Tanuj Sharma <tanujpandit16q@gmail.com>
2026-07-21 16:26:43 +0530

    restore stable document worker

--- backend/workers/documentWorker.js
+++ backend/workers/documentWorker.js
@@ -4,7 +4,6 @@ console.log("🔥 DOCUMENT WORKER STARTED");
 
 const { Worker } = require("bullmq");
 const fs = require("fs");
-const path = require("path");
 
 const redisConnection = require("../config/redis");
 const connectDB = require("../config/db");
@@ -23,313 +22,174 @@ const { uploadVectors } = require("../services/pineconeService");
 connectDB();
 
 
-
 const worker = new Worker(
 
     "document-processing",
 
     async(job)=>{
 
-        try{
+        console.log(
+            "Processing job:",
+            job.id
+        );
 
-            console.log(
-                "Processing job:",
-                job.id
+
+        const { documentId } = job.data;
+
+
+        const document = await Document.findById(
+            documentId
+        );
+
+
+        if(!document){
+
+            throw new Error(
+                "Document not found"
+            );
+
+        }
+
+
+        console.log(
+            "Processing file:",
+            document.filename
+        );
+
+
+
+        // Read PDF
+
+        const pdfBuffer = fs.readFileSync(
+            document.filePath
+        );
+
+
+        console.log(
+            "Reading PDF:",
+            document.filePath
+        );
+
+
+        console.log(
+            "File exists:",
+            fs.existsSync(document.filePath)
+        );
+
+
+
+        // Extract text
+
+        const parser = new PDFParse({
+            data: pdfBuffer
+        });
+
+
+        const pdfData = await parser.getText();
+
+
+
+        const chunks = chunkText(
+            pdfData.text
+        );
+
+
+        console.log(
+            "Total chunks:",
+            chunks.length
+        );
+
+
+
+        const vectors = [];
+
+
+
+        // Sequential embedding
+
+        for(let i = 0; i < chunks.length; i++){
+
+
+            const vector = await generateEmbedding(
+                chunks[i].content
             );
 
 
-            const { documentId } = job.data;
+            vectors.push({
 
+                id:`${documentId}_chunk_${i}`,
 
-            const document = await Document.findById(
-                documentId
-            );
+                values:vector,
 
 
-            if(!document){
+                metadata:{
 
-                throw new Error(
-                    "Document not found"
-                );
+                    documentId:
+                    documentId.toString(),
 
-            }
+                    text:
+                    chunks[i].content,
 
+                    chunkIndex:i,
 
-
-            console.log(
-                "Processing file:",
-                document.filename
-            );
-
-
-
-            const uploadDir = path.resolve(
-                __dirname,
-                "../uploads"
-            );
-
-
-            console.log(
-                "Worker upload directory:",
-                uploadDir
-            );
-
-
-            console.log(
-                "Files inside uploads:",
-                fs.existsSync(uploadDir)
-                ? fs.readdirSync(uploadDir)
-                : "Folder not found"
-            );
-
-
-
-            const filePath = path.resolve(
-                uploadDir,
-                document.filename
-            );
-
-
-
-            console.log(
-                "Reading PDF:",
-                filePath
-            );
-
-
-            console.log(
-                "File exists:",
-                fs.existsSync(filePath)
-            );
-
-
-
-            if(!fs.existsSync(filePath)){
-
-                throw new Error(
-                    "PDF file not found: " + filePath
-                );
-
-            }
-
-
-
-            // Read PDF
-
-            const pdfBuffer = fs.readFileSync(
-                filePath
-            );
-
-
-            console.log(
-                "Starting PDF text extraction..."
-            );
-
-
-            const parser = new PDFParse({
-
-                data: pdfBuffer
-
-            });
-
-
-            const pdfData = await parser.getText();
-
-
-            console.log(
-                "✅ PDF TEXT EXTRACTION COMPLETED"
-            );
-
-
-
-            const pages = pdfData.pages;
-
-
-            console.log(
-                "Total Pages:",
-                pages.length
-            );
-
-
-
-            let chunks = [];
-
-
-
-            pages.forEach(page=>{
-
-
-                const pageChunks = chunkText(
-                    page.text
-                );
-
-
-                pageChunks.forEach(chunk=>{
-
-
-                    chunks.push({
-
-                        ...chunk,
-
-                        pageNumber:
-                        page.pageNumber
-
-                    });
-
-
-                });
-
-
-            });
-
-
-
-            console.log(
-                "Total chunks:",
-                chunks.length
-            );
-
-
-            console.log(
-                "✅ CHUNK CREATION COMPLETED"
-            );
-
-
-
-            const vectors = [];
-
-
-
-            console.log(
-                "🚀 STARTING EMBEDDING GENERATION"
-            );
-
-
-
-            for(
-                let i = 0;
-                i < chunks.length;
-                i++
-            ){
-
-
-                console.log(
-                    `Generating embedding ${i+1}/${chunks.length}`
-                );
-
-
-                const vector =
-                await generateEmbedding(
-                    chunks[i].content
-                );
-
-
-                console.log(
-                    `Embedding completed ${i+1}/${chunks.length}`
-                );
-
-
-
-                vectors.push({
-
-                    id:
-                    `${documentId}_chunk_${i}`,
-
-
-                    values:
-                    vector,
-
-
-                    metadata:{
-
-                        documentId:
-                        documentId.toString(),
-
-
-                        text:
-                        chunks[i].content,
-
-
-                        chunkIndex:
-                        i,
-
-
-                        pageNumber:
-                        chunks[i].pageNumber,
-
-
-                        filename:
-                        document.filename
-
-                    }
-
-                });
-
-
-            }
-
-
-
-            console.log(
-                "Uploading vectors to Pinecone..."
-            );
-
-
-
-            await uploadVectors(
-                vectors
-            );
-
-
-
-            console.log(
-                "✅ ALL VECTORS UPLOADED"
-            );
-
-
-
-            await Document.findByIdAndUpdate(
-
-                documentId,
-
-                {
-
-                    status:"completed",
-
-                    totalChunks:
-                    chunks.length
+                    filename:
+                    document.filename
 
                 }
 
-            );
-
+            });
 
 
             console.log(
-                "🎉 DOCUMENT PROCESSING COMPLETED"
+                `Embedding ${i+1}/${chunks.length}`
             );
 
 
         }
-        catch(error){
 
 
-            console.error(
-                "❌ WORKER ERROR:",
-                error
-            );
+
+        console.log(
+            "Uploading vectors to Pinecone..."
+        );
 
 
-            throw error;
+        await uploadVectors(
+            vectors
+        );
 
-        }
+
+
+        console.log(
+            "Vectors uploaded successfully"
+        );
+
+
+
+        await Document.findByIdAndUpdate(
+
+            documentId,
+
+            {
+                status:"completed",
+                totalChunks:chunks.length
+            }
+
+        );
+
+
+
+        console.log(
+            "Document processing completed"
+        );
+
 
 
     },
 
 
     {
-        connection: redisConnection
+        connection:redisConnection
     }
 
 );
