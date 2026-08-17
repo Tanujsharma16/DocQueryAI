d0b97779c4e75c4fdff02c80413215e72a1d3c45
Tanuj Sharma <tanujpandit16q@gmail.com>
2026-08-17 21:58:41 +0530

    Not neede

--- backend/queues/documentQueue.js
+++
@@ -1,8 +0,0 @@
-const { Queue } = require("bullmq");
-const redisConnection = require("../config/redis");
-
-const documentQueue = new Queue("document-processing", {
-    connection: redisConnection
-});
-
-module.exports = documentQueue;
